edb5ef0c53a0dacae72732a8b9d393764dc4e8d4
Anjum Nasim <117526528+AnjumNasim@users.noreply.github.com>
2022-11-09 15:27:26 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,48 @@
+// let a = 4
+// // Square of a
+// let b = a**2
+// console.log(b)
+
+// // Squareroot
+// let c = Math.sqrt(b)
+// console.log(c)
+
+// // Addition
+// let x = 25
+// let y = 75
+// let z = (x+y)
+// console.log(z)
+ 
+// //subtract
+// let j = 20;
+// let h = 4
+// let g = (j-h)
+// console.log(g)
+
+// //Divide
+// let l = (j/h)
+// console.log(l)
+
+// //Percentile/Mod
+// let Anjum = (j%h)
+// console.log(Anjum)
+
+//if..else
+// let ss = 19
+
+// let yy = (ss%2)
+// console.log(yy)
+
+// Show whether a variable is Odd or Even
+
+let zz = 7
+if(zz%2 !== 0) {
+    console.log(zz + ' is odd')
+}else{
+    console.log(zz + ' is even')
+}
+
+
+
+
+
---
+++ practice1.js
@@ -0,0 +1,42 @@
+// Addition 
+let a = 859
+let b = 495
+let c = (a+b)
+console.log(c)
+
+//Subtraction
+let d = (c-b)
+console.log(d)
+
+//Multiplication
+let e = (a*3)
+console.log(e)
+
+//Division
+let f = (b/3)
+console.log(f)
+
+//Percentile/Mod
+let g = 10
+let h = 2
+let i = (g%h)
+console.log(i)
+
+//Square
+let j = a**3
+console.log(j)
+
+//Squareroot
+let k = 64
+let l = Math.sqrt(k)
+console.log(l)
+
+//Show whether the variable is odd or even 
+let x = 91
+let y = 40
+
+if (x%2 !==0) {console.log(x + ' is odd')}
+else {console.log(x + ' is even')}
+
+if (y%2 !==0) {console.log(y +  ' is odd')}
+else {console.log(y + ' is even')}
