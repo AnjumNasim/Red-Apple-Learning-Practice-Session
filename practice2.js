6ef9a49b139c49ad3b328ee490dfafb4edf6e120
Anjum Nasim <117526528+AnjumNasim@users.noreply.github.com>
2022-11-10 16:58:45 +0530

    Add files via upload

---
+++ practice2.js
@@ -0,0 +1,29 @@
+// And(&&), Or(||)
+/*let a = 20
+let b = 30
+
+if(a>=20 && b<=29) {
+    console.log('is true')
+} else {
+    console.log('is false')
+}
+
+
+if(a>=20 || b<=29) {
+    console.log('is true')
+} else {
+    console.log('is false')
+}*/
+ 
+//If..else, else if.
+let x = 25
+
+if(x>=25) {console.log('is greater than 25')}
+
+else if(x<=25) {console.log('is less than 25')}
+
+else if(x!==25) {console.log('is not equals to 25')}
+
+else{console.log('is a string')}
+
+
