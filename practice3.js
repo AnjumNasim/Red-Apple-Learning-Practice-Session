1529c65a846abb30bf219ee842eeca4c7bf262c6
Anjum Nasim <117526528+AnjumNasim@users.noreply.github.com>
2022-11-15 17:39:02 +0530

    Add files via upload

---
+++ practice3.js
@@ -0,0 +1,26 @@
+// (&&)And, (||)Or Operators
+
+let a = 20
+let b = 35
+
+/*if (a>= 8 && b<=35) {
+    console.log('True')
+} else{
+    console.log('False')
+}
+
+if (a<=8 || b>=36) {
+    console.log('true')
+} else {
+    console.log('false')
+}*/
+
+if (a>= 15 && a<=19) {
+    console.log('output 1')
+} else if (a>= 18 && a<=25) {
+    console.log('output 2')
+} else if (a>=19 || a<=10){
+    console.log('output 3')
+} else {
+    console.log('output 4')
+}
---
+++ practice4.js
@@ -0,0 +1,22 @@
+// function
+
+//Function Declaring
+/*function R(A, S) {
+    if ((A>=10 && A<=50) && (S>=10 && S<=50)){
+        return [A,S] ;
+    } 
+    else {
+        return "Fail"
+    }
+}
+console.log(R(40,35))
+*/
+
+
+//Function Declaring
+function sum(a, b) {
+    return a + b
+}
+console.log(sum(20, 30))          //calling and then printing the return
+
+
