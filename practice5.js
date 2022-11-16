8fd9eb46be039faea36427d58863e2ec853e86d7
Anjum Nasim <117526528+AnjumNasim@users.noreply.github.com>
2022-11-16 17:48:04 +0530

    Add files via upload

---
+++ practice5.js
@@ -0,0 +1,10 @@
+// function declaring
+/*function sum(a, b) {console.log(a+b)}
+
+//function calling
+sum(5, 4)*/
+
+function Divide(x, y) { 
+    return x/y 
+}
+console.log(Divide(40, 2))
---
+++ practice6.js
@@ -0,0 +1,27 @@
+/*function leapyear(a) {
+    if(a%4 ==0) [console.log(a + ' Leap Year')]
+    else [console.log(a + ' Not a Leap Year')]
+}
+leapyear(2016)*/
+
+
+
+function Result(subject1, subject2, subject3) 
+
+{
+    Total=subject1+subject2+subject3
+    if (Total>280) [console.log(Total + ' Grade A')]
+    else if (Total>200) [console.log(Total + ' Grade B')]
+    else if (Total>150) [console.log(Total + ' Grade C')]
+    else [console.log(Total + ' Fail')]
+}
+
+Result(65, 70, 50)
+
+
+
+
+
+
+
+
