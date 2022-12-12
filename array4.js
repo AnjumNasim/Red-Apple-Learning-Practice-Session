6797350129f91cd9b062f46697706f46853b9619
Anjum Nasim <117526528+AnjumNasim@users.noreply.github.com>
2022-12-12 18:06:25 +0530

    Add files via upload

---
+++ array4.js
@@ -0,0 +1,56 @@
+// // Array Practice 
+let students = [
+    {
+        Name: 'Anjum',
+        Age: 25,
+        Address: 'Kolkata,West Bengal'
+    },
+    {
+        Name: 'Saba',
+        Age: 23,
+        Address: 'Howrah, West Bengal'
+
+    },
+    {
+        Name: 'Rajdeep',
+        Age: 25,
+        Address: 'Kolkata, West Bengal'
+    }
+
+]
+
+console.log(students[0]);
+
+
+
+
+
+
+
+
+// let ary = [10,20,30,40,50,60];
+
+
+// for (a = 0; a<= ary.length; a++) {
+//     console.log(ary[a])
+// }
+// console.log(ary.length)
+
+
+
+
+// let ary = [10,20,30,40,50,60];
+
+// for (x in ary){
+//     console.log(x)
+// }
+
+
+
+
+// let a = ['q','w','e','r','t','y'];
+// for (let z of a){
+//     console.log(z)
+// }
+
+
